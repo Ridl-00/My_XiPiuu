40432afbc3b09eca9592df224593f62135d85f5c
Ridl-00 <ridl2569@gmail.com>
2024-07-10 13:15:30 +0800

    把宏定义改成了适合所需指令的样子

--- DIY.srcs/sources_1/new/defines.v
+++ DIY.srcs/sources_1/new/defines.v
@@ -49,73 +49,86 @@
 `define ChipDisable 1'b0
 
 //指令相关(op字段)
-`define EXE_AND  6'b100100
-`define EXE_OR   6'b100101
-`define EXE_XOR  6'b100110
-`define EXE_NOR  6'b100111
+
+//I型
+//逻辑
 `define EXE_ANDI 6'b001100
 `define EXE_ORI  6'b001101
-`define EXE_XORI 6'b001110
 `define EXE_LUI  6'b001111
-`define EXE_BNE  6'b000101
+//算数
+`define EXE_ADDI 6'b001000
+//装载
 `define EXE_LW   6'b100011
 `define EXE_SW   6'b101011
-
-`define EXE_SLL  6'b000000
-`define EXE_SLLV 6'b000100
-`define EXE_SRL  6'b000010
-`define EXE_SRLV 6'b000110
-`define EXE_SRA  6'b000011
-`define EXE_SRAV 6'b000111
-`define EXE_SYNC 6'b001111
-`define EXE_PREF 6'b110011
-
+`define EXE_LB   6'b100000
+`define EXE_SB   6'b101000
+//转移
+`define EXE_BNE  6'b000101
+`define EXE_BEQ  6'b000100
 `define EXE_BLEZ 6'b000110
+`define EXE_BGTZ 6'b000111
+
+//J型
+//转移
+`define EXE_J    6'b000010
+`define EXE_JAL  6'b000011
+`define EXE_JR   6'b001000
 
 
-`define EXE_NOP 6'b000000 //无操作
+`define EXE_NOP  6'b000000 //无操作
 `define SSNOP 32'b00000000000000000000000001000000
 
-`define EXE_SPECIAL_INST 6'b000000
+`define EXE_SPECIAL_INST 6'b000000 //R型指令
 `define EXE_REGIMM_INST 6'b000001
 `define EXE_SPECIAL2_INST 6'b011100
 
-//指令相关(funct字段)
+//指令相关(R型的funct字段)
+//逻辑
+`define EXE_AND  6'b100100
+`define EXE_OR   6'b100101
+`define EXE_XOR  6'b100110
+//算数
 `define EXE_ADDU 6'b100001
 `define EXE_SUB  6'b100010
+//位移
 `define EXE_SLL  6'b000000
+`define EXE_SRL  6'b000010
 `define EXE_SRLV 6'b000110
 
 
 //AluOp  Alu操作
+`define EXE_ANDI_OP  8'b01011001
+`define EXE_ORI_OP   8'b01011010
+`define EXE_LUI_OP   8'b01011100   
+
+`define EXE_ADDI_OP  8'b01010101
+
+`define EXE_LW_OP    8'b11100011
+`define EXE_SW_OP    8'b11101011
+`define EXE_LB_OP    8'b11100000
+`define EXE_SB_OP    8'b11101000
+
+`define EXE_BNE_OP   8'b01010010
+`define EXE_BEQ_OP   8'b01010001
+`define EXE_BLEZ_OP  8'b01010011
+`define EXE_BGTZ_OP  8'b01010100
+
+`define EXE_J_OP     8'b01001111
+`define EXE_JAL_OP   8'b01010000
+`define EXE_JR_OP    8'b00001000
+
+
 `define EXE_AND_OP   8'b00100100
 `define EXE_OR_OP    8'b00100101
 `define EXE_XOR_OP   8'b00100110
-`define EXE_NOR_OP   8'b00100111
-`define EXE_ANDI_OP  8'b01011001
-`define EXE_ORI_OP   8'b01011010
-`define EXE_XORI_OP  8'b01011011
-`define EXE_LUI_OP   8'b01011100   
 
-`define EXE_SLL_OP   8'b01111100
-`define EXE_SLLV_OP  8'b00000100
-`define EXE_SRL_OP   8'b00000010
-`define EXE_SRLV_OP  8'b00000110
-`define EXE_SRA_OP   8'b00000011
-`define EXE_SRAV_OP  8'b00000111
-
-`define EXE_SLT_OP   8'b00101010
-`define EXE_SLTU_OP  8'b00101011
-`define EXE_SLTI_OP  8'b01010111
-`define EXE_SLTIU_OP 8'b01011000   
-`define EXE_ADD_OP   8'b00100000
 `define EXE_ADDU_OP  8'b00100001
 `define EXE_SUB_OP   8'b00100010
-`define EXE_SUBU_OP  8'b00100011
-`define EXE_ADDI_OP  8'b01010101
-`define EXE_ADDIU_OP 8'b01010110
-`define EXE_CLZ_OP   8'b10110000
-`define EXE_CLO_OP   8'b10110001
+
+`define EXE_SLL_OP   8'b01111100
+`define EXE_SRL_OP   8'b00000010
+`define EXE_SRLV_OP  8'b00000110
+
 
 `define EXE_NOP_OP 8'b00000000 //无
 
@@ -123,8 +136,9 @@
 `define EXE_RES_LOGIC 3'b001 //逻辑操作模式
 `define EXE_RES_SHIFT 3'b010 //选择移位操作
 `define EXE_RES_ARITHMETIC 3'b100 //算数指令模式
+`define EXE_RES_MOVE 3'b011 //数据移动（装载指令模式）
 
-`define EXE_RES_NOP 3'b00
+`define EXE_RES_NOP 3'b000
 
 //指令存储器inst_rom
 `define InstAddrBus 31:0 //ROM的地址总线宽度
